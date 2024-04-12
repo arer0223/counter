f35910f0da8c9a5bbc9a57f4e1a76e5e29433690
Arvid Eriksson <arvid.eriksson@skola.sala.se>
2024-04-12 11:01:24 +0200

    Add project files.

---
+++ counter/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        counter counter = new counter();
+        counter.Increment();
+        counter.Increment();
+        counter.Increment();
+        // ska skriva ut 3
+        Console.WriteLine("counter: " + counter.GetValue());
+        counter.Reset();
+        // ska skriva ut 0
+        Console.WriteLine("counter: " + counter.GetValue());
+    }
+    }
+}
---
+++ counter/counter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace counter
+{
+    internal class counter
+    {
+        // TODO lite variabler
+        int value = 0;
+        // ökar räknaren med ett
+        public void Increment()
+        {
+            value++; 
+        }
+
+        // återställer värdet till noll
+        public void Reset()
+        {
+            value = 0; 
+        }
+
+        // ger värdet på räknaren
+        public int GetValue()
+        {
+            // TODO
+            return value;
+        }
+    }
+}
